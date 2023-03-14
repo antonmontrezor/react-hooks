1fa6ed7db7be2bdb688ae8a17b196a54385022a5
antonmontrezor <montrezoranton991@gmail.com>
2023-03-14 12:56:48 -0400

    finish block 6

---
+++ src/error-boundary.js
@@ -0,0 +1,19 @@
+import * as React from 'react'
+
+class ErrorBoundary extends React.Component {
+  state = {error: null}
+  static getDerivedStateFromError(error) {
+    return {error}
+  }
+
+  render() {
+    const {error} = this.state
+    if (error) {
+      return <this.props.FallbackComponent error={error} />
+    }
+
+    return this.props.children
+  }
+}
+
+export default ErrorBoundary
